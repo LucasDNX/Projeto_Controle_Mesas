cccc84da4e9022b8b17d493dec271aafa80da732
Joao antonio <ajoao0779@gmail.com>
2024-05-14 19:03:16 -0300

    Criação das classes

---
+++ ProjetoControleMesas/Models/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoControleMesas;
+
+public class Cliente
+{
+    public Cliente()
+    {
+        CriadoEm = DateTime.Now;
+        Id = Guid.NewGuid().ToString();
+    }
+
+    public Cliente(string nome, string endereco, string telefone)
+    {
+        Id = Guid.NewGuid().ToString();
+        Nome = nome;
+        Endereco = endereco;
+        Telefone = telefone;
+        CriadoEm = DateTime.Now;
+    }
+
+    
+    public string Id { get; set; }
+    
+    [Required(ErrorMessage = "Este campo é obrigatório!")]
+    public string Nome { get; set; }
+
+    public string? Endereco { get; set; }
+
+    [Required(ErrorMessage = "Este campo é obrigatório!")]
+    public string Telefone { get; set; }
+    
+    public DateTime CriadoEm { get; set; }
+
+}
+
